33d4d0c7201cf9c39948e7eeec01511742ef62b9
SeanMee86 <sean.mee86@yahoo.com>
2020-05-09 15:06:32 -0700

    working on decorators

---
+++ decorators/src/app.ts
@@ -0,0 +1,17 @@
+function Logger(constructor: Function) {
+    console.log('Logging...');
+    console.log(constructor);
+}
+
+@Logger
+class Person {
+    name = 'Sean';
+
+    constructor() {
+        console.log('Creating Person Object...')
+    }
+}
+
+const pers = new Person();
+
+console.log(pers);

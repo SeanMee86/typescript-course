fb9915fab6fd29fe25bf8e1cb1fd4a3096ecc33c
hss-smee <seanmee8186@gmail.com>
2020-05-10 07:06:56 -0700

    starting course project

--- decorators/dist/app.js
+++ decorators/dist/app.js
@@ -7,23 +7,20 @@
 // }
 //
 // function WithTemplate(template: string, hookId: string) {
-//     return function(constructor: any) {
-//         const hookEl = document.getElementById(hookId);
-//         const p = new constructor();
-//         console.log(hookEl)
-//         if(hookEl) {
-//             hookEl.innerHTML = template;
-//             hookEl.querySelector('h1')!.textContent = p.name;
+//     return function<T extends { new(...args: any[]): {name: string} }>(originalConstructor: T) {
+//         return class extends originalConstructor {
+//             constructor(..._: any[]){
+//                 super();
+//                 const hookEl = document.getElementById(hookId);
+//                 if(hookEl) {
+//                     hookEl.innerHTML = template;
+//                     hookEl.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
 //         }
-//         console.log('activated')
 //     }
 // }
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
+//
 // @Logger('Logging - Person')
 // @WithTemplate('<h1>My Person Object</h1>', 'app')
 // class Person {
@@ -35,28 +32,88 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 // }
 //
 // const pers = new Person();
+//
 // console.log(pers);
-function Log(target, propertyName) {
-    console.log('Propterty Decorator');
-    console.log(target, propertyName);
+//
+// function Log(target: any, propertyName: string | Symbol) {
+//     console.log('Propterty Decorator');
+//     console.log(target, propertyName);
+// }
+//
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//     console.log('Accessor Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+//
+// function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+//     console.log('Method Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+//
+// function Log4(target: any, name: string | Symbol, position: number) {
+//     console.log('Parameter Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(position);
+// }
+//
+// class Product {
+//     @Log
+//     title: string;
+//     private _price: number;
+//
+//     @Log2
+//     set price(val: number) {
+//         if(val > 0){
+//             this._price = val;
+//         }else{
+//             throw new Error('Invalid Price should be positive');
+//         }
+//     }
+//
+//     @Log3
+//     getPriceWithTax(@Log4 tax: number) {
+//         return this.price * (1 + tax);
+//     }
+//
+//     constructor(public t: string, private p: number) {
+//         this.title = t;
+//         this._price = p;
+//     }
+// }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function AutoBind(_, _2, descriptor) {
+    console.log(descriptor);
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get: function () {
+            return originalMethod.bind(this);
+        }
+    };
+    return adjDescriptor;
 }
-class Product {
-    constructor(title, _price) {
-        this.title = title;
-        this._price = _price;
+class Printer {
+    constructor() {
+        this.message = 'This works';
     }
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error('Invalid Price should be positive');
-        }
-    }
-    getPriceWithTax(tax) {
-        return this.price * (1 + tax);
+    showMessage() {
+        return this.message;
     }
 }
 __decorate([
-    Log
-], Product.prototype, "price", null);
+    AutoBind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector('button');
+button.addEventListener('click', p.showMessage);
--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -6,18 +6,20 @@
 // }
 //
 // function WithTemplate(template: string, hookId: string) {
-//     return function(constructor: any) {
-//         const hookEl = document.getElementById(hookId);
-//         const p = new constructor();
-//         console.log(hookEl)
-//         if(hookEl) {
-//             hookEl.innerHTML = template;
-//             hookEl.querySelector('h1')!.textContent = p.name;
+//     return function<T extends { new(...args: any[]): {name: string} }>(originalConstructor: T) {
+//         return class extends originalConstructor {
+//             constructor(..._: any[]){
+//                 super();
+//                 const hookEl = document.getElementById(hookId);
+//                 if(hookEl) {
+//                     hookEl.innerHTML = template;
+//                     hookEl.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
 //         }
-//         console.log('activated')
 //     }
 // }
-
+//
 // @Logger('Logging - Person')
 // @WithTemplate('<h1>My Person Object</h1>', 'app')
 // class Person {
@@ -29,28 +31,83 @@
 // }
 //
 // const pers = new Person();
-
+//
 // console.log(pers);
+//
+// function Log(target: any, propertyName: string | Symbol) {
+//     console.log('Propterty Decorator');
+//     console.log(target, propertyName);
+// }
+//
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//     console.log('Accessor Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+//
+// function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+//     console.log('Method Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(descriptor);
+// }
+//
+// function Log4(target: any, name: string | Symbol, position: number) {
+//     console.log('Parameter Decorator');
+//     console.log(target);
+//     console.log(name);
+//     console.log(position);
+// }
+//
+// class Product {
+//     @Log
+//     title: string;
+//     private _price: number;
+//
+//     @Log2
+//     set price(val: number) {
+//         if(val > 0){
+//             this._price = val;
+//         }else{
+//             throw new Error('Invalid Price should be positive');
+//         }
+//     }
+//
+//     @Log3
+//     getPriceWithTax(@Log4 tax: number) {
+//         return this.price * (1 + tax);
+//     }
+//
+//     constructor(public t: string, private p: number) {
+//         this.title = t;
+//         this._price = p;
+//     }
+// }
 
-function Log(target: any, propertyName: string | Symbol) {
-    console.log('Propterty Decorator');
-    console.log(target, propertyName);
-}
-
-class Product {
-    @Log
-    set price(val: number) {
-        if(val > 0){
-            this._price = val;
-        }else{
-            throw new Error('Invalid Price should be positive');
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    console.log(descriptor);
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get: function() {
+            return originalMethod.bind(this);
         }
     }
+    return adjDescriptor;
+}
 
-    getPriceWithTax(tax: number) {
-        return this.price * (1 + tax);
-    }
-
-    constructor(public title: string, private _price: number) {
+class Printer {
+    message: string = 'This works';
+    @AutoBind
+    showMessage() {
+        return this.message;
     }
 }
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', p.showMessage);
